230a793d6cab47d24c22b546a5e6610e0da11a2b
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-09-18 17:57:39 -0400

    added navbar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavLink,
+    NavItem,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
 
 function App() {
     return (
         <Router>
             <div>
                 <Route path='/' exact component={Home} />
-                <Route path='/about' component={About} />
+                <Route path='/about' exact component={About} />
             </div>
         </Router>
     )
@@ -29,4 +41,83 @@ function About() {
     )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+export default class Main_Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+
+    this.state = {
+      dropdownOpen: false
+    };
+
+    this.state = {
+      isOpen: false
+    };
+
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  onMouseEnter() {
+    this.setState({dropdownOpen: true});
+  }
+
+  onMouseLeave() {
+    this.setState({dropdownOpen: false});
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md" >
+          <NavbarBrand href="/">Ecommerce</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto " navbar>
+              <NavItem>
+                <NavLink href="/men/">Men</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#">Women</NavLink>
+              </NavItem>
+              <UncontrolledDropdown onMouseOver={this.onMouseEnter} 
+                                    onMouseLeave={this.onMouseLeave} 
+                                    isOpen={this.state.dropdownOpen} 
+                                    toggle={this.toggle} >
+                <DropdownToggle nav caret>
+                  Kids
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Boys
+                  </DropdownItem>
+                  <DropdownItem>
+                    Girls
+                  </DropdownItem>
+                  
+                  </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main_Navbar />, document.getElementById('root'));
