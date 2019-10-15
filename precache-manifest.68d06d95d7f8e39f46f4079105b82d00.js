388695ac558230c04266e47da08a7a910561f128
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-10-14 21:03:06 -0400

    Updates

---
+++ precache-manifest.68d06d95d7f8e39f46f4079105b82d00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710581caefc31fb0b6160cb5e33f454e",
+    "url": "/ecommerce_site/index.html"
+  },
+  {
+    "revision": "01bf2d821e8bf4e6b71f",
+    "url": "/ecommerce_site/static/css/main.4e87b03b.chunk.css"
+  },
+  {
+    "revision": "300a5debdc976e5bf713",
+    "url": "/ecommerce_site/static/js/2.25b1c4fe.chunk.js"
+  },
+  {
+    "revision": "01bf2d821e8bf4e6b71f",
+    "url": "/ecommerce_site/static/js/main.3265d9e7.chunk.js"
+  },
+  {
+    "revision": "f9c7058932bbf783004a",
+    "url": "/ecommerce_site/static/js/runtime~main.b73af2f2.js"
+  }
+]);
